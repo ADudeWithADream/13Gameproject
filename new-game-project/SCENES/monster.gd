c504156aeb07dd58cc4c847b5b79904a1d4eb487
ADudeWithADream <lfaimanifo@students.mags.school.nz>
2025-10-08 10:42:03 +1300

    developing monster mechanics

--- new-game-project/SCENES/monster.gd
+++ new-game-project/SCENES/monster.gd
@@ -1,8 +1,8 @@
 extends StaticBody3D
 
-var arr = [$monsterposition1, $monsterposition2, $monsterposition3]
+var arr = [%monsterposition1, %monsterposition2, %monsterposition3]
 var idx = randi_range(0, arr.size() - 1)
-print(arr[idx])
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,4 +11,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#position = global_position == arr[idx].global_position
 	pass
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
+	position = global_position == arr[idx].global_position
